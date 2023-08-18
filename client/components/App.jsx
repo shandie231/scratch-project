b427bbfd8fb67be2efc03811b5abcd187ed60277
Katherine Fry <katgfry@me.com>
2023-08-18 19:46:45 -0400

    webpack and basic setup

---
+++ client/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+const App = () =>{
+    return (
+        <div>
+            <div>
+                <h1>Hello you are in App component</h1>
+            </div>
+        </div>
+    )
+}
+
+export default App;
